a5ccc07c1bebf38c40869b978cb543f99622cef0
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-03-28 15:23:21 -0400

    made backdrop blurry adjusted background color and made hr and nav sticky and positioned to top of page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,14 @@
-import name from "../../assets/Solibee-name.png";
-import gitLogo from "../../assets/github-mark.png";
-import gitLogoWhite from "../../assets/github-mark-white.png";
-import sunIcon from "../../assets/sun-svgrepo-com.svg";
-import nightIcon from "../../assets/night-svgrepo-com.svg";
-import Search from "./Search";
+import name from '../../assets/Solibee-name.png';
+import gitLogo from '../../assets/github-mark.png';
+import gitLogoWhite from '../../assets/github-mark-white.png';
+import sunIcon from '../../assets/sun-svgrepo-com.svg';
+import nightIcon from '../../assets/night-svgrepo-com.svg';
+import Search from './Search';
 
 export default function NavBar() {
   return (
-    <div>
-      <div class="sticky text-sm font-medium text-slate-700  top-0 w-full flex justify-between items-center px-5 py-2">
+    <div class="sticky backdrop-blur-sm top-0 z-50 bg-white-100">
+      <div class="text-sm font-medium text-slate-700  top-0 w-full flex justify-between items-center px-5 py-2">
         <div class="flex items-center">
           <img
             class="h-8 mr-6"
@@ -33,7 +33,7 @@ export default function NavBar() {
           <img class="h-6 ml-6 " src={sunIcon} alt="Sun icon" />
         </div>
       </div>
-      <hr class="border-1 border-amber-300" />
+      <hr class="border-1 border-orange-100" />
     </div>
   );
 }
