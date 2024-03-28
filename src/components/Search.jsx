cc2de213f731c35292c73ce9035af7b7d0bcf65a
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-03-28 15:23:56 -0400

    change color of search button to custom webpage colors

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,7 @@ import search from "../../assets/search-5-svgrepo-com.svg";
 export default function Search() {
   return (
     <div class="w-40">
-      <button class="flex items-center text-slate-700 hover:bg-orange-50 border border-orange-200 leading-6 py-1.5 px-1 w-full rounded-md text-sm">
+      <button class="flex items-center text-slate-700 hover:bg-orange-50 border border-orange-100 leading-6 py-1.5 px-1 w-full rounded-md text-sm">
         <img class="h-7 px-1" src={search} alt="search icon" />
         Search ...
       </button>
