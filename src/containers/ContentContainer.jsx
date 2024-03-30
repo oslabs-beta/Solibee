9524da208da1aef22d478ce80f6ef19990e09d56
Neul Seo <neul.seo.1213@gmail.com>
2024-03-30 13:27:34 -0400

    added step counters in guide

--- src/containers/ContentContainer.jsx
+++ src/containers/ContentContainer.jsx
@@ -43,11 +43,22 @@ export default function ContentContainer() {
           relative pl-10 before:z-10 before:content-[counter(step)] before:absolute before:left-0 before:flex before:items-center before:justify-center before:w-[calc(1.375rem+1px)] before:h-[calc(1.375rem+1px)] before:text-[0.9rem]
 before:font-bold before:text-slate-700 before:rounded-md before:shadow-md before:ring-2 before:ring-slate-900/5 before:z-20"
           >
-            Run the following command
+            Install the following dependencies:
           </p>
-
-<!
-
+          <p
+            class="step-counter font-heading mt-8 text-xl font-semibold tracking-tight
+          relative pl-10 before:z-10 before:content-[counter(step)] before:absolute before:left-0 before:flex before:items-center before:justify-center before:w-[calc(1.375rem+1px)] before:h-[calc(1.375rem+1px)] before:text-[0.9rem]
+before:font-bold before:text-slate-700 before:rounded-md before:shadow-md before:ring-2 before:ring-slate-900/5 before:z-20"
+          >
+            Copy and paste the following code into your project
+          </p>
+          <p
+            class="step-counter font-heading mt-8 text-xl font-semibold tracking-tight
+          relative pl-10 before:z-10 before:content-[counter(step)] before:absolute before:left-0 before:flex before:items-center before:justify-center before:w-[calc(1.375rem+1px)] before:h-[calc(1.375rem+1px)] before:text-[0.9rem]
+before:font-bold before:text-slate-700 before:rounded-md before:shadow-md before:ring-2 before:ring-slate-900/5 before:z-20"
+          >
+            Update the import paths to match your project setup
+          </p>
         </div>
       </div>
 
