19d8844e2271032d099d7f18dd44ef1ebbb19dbf
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-01 11:15:59 -0400

    eslint changes & added Code component to page

--- src/components/ContentComponent.jsx
+++ src/components/ContentComponent.jsx
@@ -1,10 +1,48 @@
+import { codeToHtml } from "shiki";
+import { createSignal, createResource } from "solid-js";
+import Code from "./Code";
+
+const code = `export default function Search() {
+  return (
+    <div class="w-40">
+      <button>
+        <img class="h-7 px-1" src={search} alt="search icon" />
+        Search ...
+      </button>
+    </div>
+  );
+}`;
+
 export default function ContentComponent() {
+
+  const [formattedCode, setCodeHtml] = createSignal();
+  
+  const getHtml = async () => {
+    //try token code 
+    let codeHtml = await codeToHtml(code, {
+      lang: "jsx",
+      theme: "vesper",
+    });
+    console.log("I am codeHtml", codeHtml);
+    console.log("I am type", typeof codeHtml);
+    
+    setCodeHtml(codeHtml);
+    return codeHtml;
+  };
+  getHtml();
+
+  // const [htmlCode] = createResource(getHtml);
+  // console.log('I am from createResource', htmlCode);
+  // setCodeHtml(htmlCode);
+
   return (
     <div class="max-w-3xl mx-auto pt-10 xl:max-w-none w-10/12">
       {/* Component name and description */}
       <header class="">
-        <h1 class="inline-block text-4xl font-bold tracking-tight">Acordion</h1>
-        <hr></hr>
+        <h1 class="inline-block text-4xl font-bold tracking-tight">
+          Accordion
+        </h1>
+        <hr />
         <p class="text-slate-500">
           A vertically stacked set of interactive headings that each reveal a
           section of content.
@@ -16,15 +54,15 @@ export default function ContentComponent() {
         {/* class="mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 relative rounded-md border" */}
         <div class="w-1/2 my-5">
           <p class="text-slate-500">Code</p>
-          <hr></hr>
+          <hr />
           <div class="flex min-h-[350px] w-full justify-center items-center bg-slate-100">
-            Content
+            <Code html = {formattedCode()}/>
           </div>
         </div>
 
         <div class="w-1/2 my-5">
           <p class="text-slate-500">Preview</p>
-          <hr></hr>
+          <hr />
           <div class="flex min-h-[350px] w-full justify-center items-center bg-slate-100">
             Content
           </div>
@@ -36,7 +74,7 @@ export default function ContentComponent() {
         <p class="text-slate-500 text-2xl tracking-tight" id="installation">
           Installation Guide
         </p>
-        <hr></hr>
+        <hr />
         <div class="steps mb-12 ml-4 border-l pl-8 [counter-reset:step]">
           <p class="font-heading mt-8 scroll-m-20 text-xl font-semibold tracking-tight">
             Run the following command
@@ -53,18 +91,18 @@ export default function ContentComponent() {
         <p class="text-slate-500 text-2xl tracking-tight" id="Usage">
           Usage
         </p>
-        <hr></hr>
+        <hr />
         <div>
-          <p></p>
+          <p />
         </div>
       </div>
       <div class="my-5 flex flex-col mt-12">
         <p class="text-slate-500 text-2xl tracking-tight" id="installation">
           Test
         </p>
-        <hr></hr>
+        <hr />
         <div>
-          <p></p>
+          <p />
         </div>
       </div>
     </div>
