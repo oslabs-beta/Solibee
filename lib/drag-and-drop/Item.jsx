473b58d5fdbd02a635fdd80262c13e21a0b7bd5a
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-04-02 12:49:05 -0400

    Add remove item button

--- lib/drag-and-drop/Item.jsx
+++ lib/drag-and-drop/Item.jsx
@@ -5,12 +5,18 @@ export default (props) => {
 
   return (
     <div
-      class='border border-black m-3 p-3 cursor-move'
+      class='border border-black m-3 p-3 cursor-move flex justify-between items-center'
       draggable={true}
       onDragStart={(e) => handleDragStart(e)}
     >
       {props.items.filter((i) => i.itemID == props.itemID)[0].title}{' '}
       {props.itemID}
+      <button
+        class='border border-black m-1 p-1'
+        onClick={() => props.updateItems('delete', { itemID: props.itemID })}
+      >
+        X
+      </button>
     </div>
   );
 };
