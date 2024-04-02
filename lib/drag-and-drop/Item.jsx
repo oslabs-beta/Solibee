fa66d82e731291cfec621013bdbb7b82c9e2020a
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-04-02 17:57:23 -0400

    update item locations on mount

--- lib/drag-and-drop/Item.jsx
+++ lib/drag-and-drop/Item.jsx
@@ -1,10 +1,21 @@
+import { onMount } from 'solid-js';
+
 export default (props) => {
+  let ref;
+
+  onMount(() => {
+    const rect = ref.getBoundingClientRect();
+    const y = (rect.top + rect.bottom) / 2;
+    props.setItemLocations({ ...props.itemLocations, [props.itemID]: y });
+  });
+
   const handleDragStart = (e) => {
     e.dataTransfer.setData('id', props.itemID);
   };
 
   return (
     <div
+      ref={ref}
       class='border border-black m-3 p-3 cursor-move flex justify-between items-center'
       draggable={true}
       onDragStart={(e) => handleDragStart(e)}
