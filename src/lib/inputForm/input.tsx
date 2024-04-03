4b9041c61ae6fd665515842fdb1892d0ce6ee2f3
Congke Zhao <congkezhao@s-MacBook-Pro.local>
2024-04-02 20:09:20 -0400

    first push input form

---
+++ src/lib/inputForm/input.tsx
@@ -0,0 +1,41 @@
+import { createSignal } from 'solid-js';
+
+function InputForm() {
+  const [name, setName] = createSignal('');
+  const [email, setEmail] = createSignal('');
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    console.log('Submitted:', { name: name(), email: email() });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label for='name'>Name:</label>
+        <input
+          id='name'
+          type='text'
+          placeholder='solidbee'
+          value={name()}
+          onInput={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label for='email'>Email:</label>
+        <input
+          id='email'
+          type='email'
+          placeholder='solidbee@gmail.com'
+          value={email()}
+          onInput={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <button type='submit'>Submit</button>
+    </form>
+  );
+}
+
+export default InputForm;
