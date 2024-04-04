c0f2c2ca86b94cc9e0cc38d72d7133935e0ea270
Neul Seo <neul.seo.1213@gmail.com>
2024-04-03 21:55:29 -0400

    added div to include components indicator and a >

--- src/components/ContentComponent.jsx
+++ src/components/ContentComponent.jsx
@@ -38,8 +38,15 @@ export default function ContentComponent(props) {
 
   return (
     // previously existed: xl:max-w-none
-    <div class='prose max-w-[800px] mt-10 mx-auto w-10/12 bg-white/[0.9] overflow-auto'>
+    <div class='prose max-w-[800px] min-w-0 mt-10 mx-auto w-10/12 bg-white/[0.9] overflow-auto'>
       {/* Component name and description */}
+      <div class="mb-4 max-h-8 flex items-center text-sm">
+        <div class="">Components</div>
+        <svg class="h-4 w-4">
+          <path d="M6.1584 3.13508C6.35985 2.94621 6.67627 2.95642 6.86514 3.15788L10.6151 7.15788C10.7954 7.3502 10.7954 7.64949 10.6151 7.84182L6.86514 11.8418C6.67627 12.0433 6.35985 12.0535 6.1584 11.8646C5.95694 11.6757 5.94673 11.3593 6.1356 11.1579L9.565 7.49985L6.1356 3.84182C5.94673 3.64036 5.95694 3.32394 6.1584 3.13508Z"></path>
+        </svg>
+        {/* <div>Drawer</div> */}
+      </div>
       <header class=''>
         <h1 class='inline-block text-4xl font-bold tracking-tight'>Drawer</h1>
         <hr />
