51dc92cd594eb04c2e05448c6015f47211e9bad4
tenn501 <158115485+tenn501@users.noreply.github.com>
2024-04-05 12:34:05 -0400

    Boiler plate refactoring done.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,23 @@
-import { createSignal, useContext } from "solid-js";
-import name from "../../assets/Solibee-name.png";
-import gitLogo from "../../assets/github-mark.png";
-import gitLogoWhite from "../../assets/github-mark-white.png";
-import sunIcon from "../../assets/sun-svgrepo-com.svg";
-import nightIcon from "../../assets/night-svgrepo-com.svg";
-import Search from "./Search";
-import { ComponentContext } from "../context/ComponentContext";
+import { createSignal, useContext } from 'solid-js';
+import solibeeLogo from '../../assets/Solibee-name.png';
+import gitLogo from '../../assets/github-mark.png';
+import gitLogoWhite from '../../assets/github-mark-white.png';
+import sunIcon from '../../assets/sun-svgrepo-com.svg';
+import nightIcon from '../../assets/night-svgrepo-com.svg';
+import Search from './Search';
+import { ComponentContext } from '../context/ComponentContext';
 
 export default function NavBar() {
   const { components } = useContext(ComponentContext);
-  const href = `/component/${components[0].toLowerCase().replaceAll(" ", "")}`;
+  const href = `/component/${components[0].toLowerCase().replaceAll(' ', '')}`;
 
   return (
     <div class="bg-white-200 sticky top-0 z-50 h-16 backdrop-blur-sm">
-      <div class="top-0 flex h-5/6  w-full items-center justify-between px-5 py-2 text-sm font-medium text-slate-700">
+      <div class="flex h-5/6  w-full items-center justify-between px-5 py-2 text-sm font-medium text-slate-700">
         <div class="flex items-center">
           <img
             class="mr-6 h-8"
-            src={name}
+            src={solibeeLogo}
             alt="The word SOLIBEE with a bee on top of the letter I"
           />
           <span class="mr-6 mt-1 hover:text-orange-200">
