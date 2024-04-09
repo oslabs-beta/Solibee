18d0c0174822e7a02b81077a1e38585b02cb6557
tenn501 <158115485+tenn501@users.noreply.github.com>
2024-04-09 14:14:40 -0400

    genOtp test started

--- src/__test__/GenerateOTP.test.tsx
+++ src/__test__/GenerateOTP.test.tsx
@@ -1,17 +1,28 @@
 import GenerateOTP from '../lib/inputForm/GenerateOTP';
-import { render } from '@solidjs/testing-library';
+import { render, cleanup } from '@solidjs/testing-library';
 import '@testing-library/jest-dom';
 
 describe('GenerateOTP', () => {
-  test("renders component with correct text", () => {
-    // Render the component
-    const { getByText, unmount } = render(() => (
-      <GenerateOTP />
-    ));
 
+  let getByText: (text: string) => HTMLElement;
+
+  beforeEach(() => {
+    const result = render(() => <GenerateOTP />);
+    getByText = result.getByText;
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  it ('renders component with correct text', () => {
     // Verify if the component renders with the correct text
     expect(getByText('Regenerate')).toBeInTheDocument();
-    unmount();
+    expect(getByText('Copy')).toBeInTheDocument();
   });
+
+  it ('regenerate a new OTP and displays it each time the button is clicked', () => {
+
+  })
 });
   
