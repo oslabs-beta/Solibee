f9a78cd0a4fad89b483d5ed5388fc0951b1a50ad
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-04-12 17:55:21 -0400

    add homepage to routing in ContentContainer

--- src/containers/ContentContainer.jsx
+++ src/containers/ContentContainer.jsx
@@ -1,24 +1,26 @@
-import { createSignal, For, createEffect } from "solid-js";
-import { Router, Route } from "@solidjs/router";
-import Installation from "../components/Installation";
-import Introduction from "../components/Introduction";
-import ContentComponent from "../components/ContentComponent";
-import { useContext } from "solid-js";
-import { ComponentContext } from "../context/ComponentContext";
+import { createSignal, For, createEffect } from 'solid-js';
+import { Router, Route } from '@solidjs/router';
+import Installation from '../components/Installation';
+import Introduction from '../components/Introduction';
+import Homepage from '../components/Homepage';
+import ContentComponent from '../components/ContentComponent';
+import { useContext } from 'solid-js';
+import { ComponentContext } from '../context/ComponentContext';
 
 export default function ContentContainer() {
   const { components } = useContext(ComponentContext);
 
   return (
     // added margin bottom here
-    <div class="mx-auto mb-80 w-10/12 max-w-3xl overflow-auto pt-10 xl:max-w-none">
+    <div class='mx-auto mb-80 w-10/12 max-w-3xl overflow-auto pt-10 xl:max-w-none'>
       <Router>
-        <Route path="/installation" component={Installation} />
-        <Route path="/introduction" component={Introduction} />
+        <Route path='/' component={Homepage} />
+        <Route path='/installation' component={Installation} />
+        <Route path='/introduction' component={Introduction} />
         <For each={components}>
           {(component) => (
             <Route
-              path={`/component/${component.toLowerCase().replaceAll(" ", "")}`}
+              path={`/component/${component.toLowerCase().replaceAll(' ', '')}`}
               component={() => <ContentComponent component={component} />}
             />
           )}
