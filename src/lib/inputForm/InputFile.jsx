c2406917b132d685d8787308d7a7e0e7cb8579e0
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-13 15:56:42 -0400

    Styling changes.

--- src/lib/inputForm/InputFile.jsx
+++ src/lib/inputForm/InputFile.jsx
@@ -20,10 +20,23 @@ function InputFile() {
   };
 
   return (
-    <div>
-      <input class="" type='file' onChange={handleFileChange} />
-      <button class="bg-orange-100 rounded-md	p-1 px-2" onClick={handleSubmit}>Upload</button>
-    </div>
+    <form>
+      <div class="flex items-baseline gap-2">
+        <label for="fileInput" class="mb-2 block p-2">
+          Choose a file:
+        </label>
+        <input
+          class="rounded-md border border-gray-300 bg-white p-2"
+          id="fileInput"
+          name="fileInput"
+          type="file"
+          onChange={handleFileChange}
+        />
+        <button class="rounded-md bg-orange-100	py-1 px-2" onClick={handleSubmit}>
+          Upload
+        </button>
+      </div>
+    </form>
   );
 }
 
