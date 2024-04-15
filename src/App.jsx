136250f26076f9efd5e17f864c294f64cad5da68
Neul Seo <neul.seo.1213@gmail.com>
2024-04-14 22:22:39 -0400

    testing dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import NavBar from './components/NavBar';
-import MainContainer from './containers/MainContainer';
-import Footer from './components/Footer';
+import NavBar from "./components/NavBar";
+import MainContainer from "./containers/MainContainer";
+import Footer from "./components/Footer";
 
 function App() {
   // list of the components;
-  const components = ['Drawer', 'Kanban Card', 'Input Form'];
+  const components = ["Drawer", "Kanban Card", "Input Form"];
 
   return (
     <div
-      class="relative bg-cover bg-fixed bg-center min-h-screen"
+      class="dark relative min-h-screen bg-cover bg-fixed bg-center"
       style="background-image: url('../assets/494-[Converted].png')"
     >
       <NavBar />
