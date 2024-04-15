c91d7f20c88e590286258e070fbbadbe36557ccd
Neul Seo <neul.seo.1213@gmail.com>
2024-04-14 21:59:20 -0400

    added darkMode property in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './lib/**/*.{js,jsx,ts,tsx}'],
+  darkMode: ['selector', '[data-mode="dark"]'],
   theme: {
     extend: {
       colors: {
