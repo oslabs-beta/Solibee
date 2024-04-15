7223e3febaba54781cde1914e6a5d749c2973e59
Neul Seo <neul.seo.1213@gmail.com>
2024-04-14 23:16:01 -0400

    removed unused code, left logic for theme change in comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import { onCleanup } from "solid-js";
 import NavBar from "./components/NavBar";
 import MainContainer from "./containers/MainContainer";
 import Footer from "./components/Footer";
 
 function App() {
-  // list of the components;
-  const components = ["Drawer", "Kanban Card", "Input Form"];
+  // // Set theme in localStorage
+  // localStorage.setItem("theme", "light");
+
+  // // Retrieve theme from localStorage
+  // const selectedTheme = localStorage.getItem("theme") || "light";
+
+  // // Apply theme to the body
+  // document.body.classList.add(selectedTheme);
+
+  // // Clean up theme class on component unmount
+  // onCleanup(() => {
+  //   document.body.classList.remove(selectedTheme);
+  // });
+
+  // useEffect(() => {
+  //   localStorage.setItem("theme", "light");
+
+  //   const selectedTheme = localStorage.getItem("theme");
+
+  //   if (selectedTheme) {
+  //     document.body.classList.add(selectedTheme);
+  //   } else if (selectedTheme === "dark") {
+  //     document.body.classList.add("dark");
+  //   } else {
+  //     document.body.classList.add("light");
+  //   }
+  // });
 
   return (
     <div
