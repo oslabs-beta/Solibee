ac4bb1fe29fdabeea88f91a8c8b30583a6b996d9
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-04-15 13:21:35 -0400

    Simplify footer with border instead of hr element

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 export default function Footer() {
   return (
-    <footer class='bg-white-200/30 left-0 mx-auto w-full flex-col items-center justify-center px-5 py-5 text-sm font-medium text-slate-700 backdrop-blur-sm'>
-      <hr class='border-1 mb-5 border-orange-100' />
+    <footer class=' bg-white-200/30 left-0 mx-auto w-full flex-col items-center justify-center border-t border-orange-100 px-5 py-5 text-sm font-medium text-slate-700 backdrop-blur-sm'>
       <h3 class='flex justify-center'>
         Built by Solibee. Source code available on{' '}
         <a
