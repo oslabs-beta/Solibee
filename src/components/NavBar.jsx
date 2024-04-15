2a83efa25ed9b23f102e36daed13a457e241f5e3
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-04-15 13:09:37 -0400

    Simplify NavBar and add Solibee name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,38 +12,39 @@ export default function NavBar() {
   const href = `/component/${components[0].toLowerCase().replaceAll(' ', '')}`;
 
   return (
-    <div class='bg-white-200 sticky top-0 z-50 h-16 backdrop-blur-sm'>
-      <div class='flex h-5/6  w-full items-center justify-between px-5 py-2 text-sm font-medium text-slate-700'>
-        <div class='flex items-center'>
-          <a href='/'>
-            <img
-              class='mr-6 h-8'
-              src={solibeeLogo}
-              alt='the solibee logo: a bee in a honeycomb'
-            />
-          </a>
-          <span class='mr-6 mt-1 hover:text-orange-200'>
-            <a href='/introduction'>Docs</a>
-          </span>
-          <span class='mt-1 hover:text-orange-200'>
-            <a href={href}>Components</a>
-          </span>
-        </div>
-        <div class='mt-1 flex items-center'>
-          <Search />
-          <a
-            class='ml-6'
-            target='_blank'
-            href='https://github.com/oslabs-beta/solibee/'
-          >
-            <span class=''>
-              <img class='h-6' src={gitLogo} alt='GitHub logo' />
-            </span>
-          </a>
-          <img class='ml-6 h-6 ' src={sunIcon} alt='Sun icon' />
-        </div>
+    <div class='bg-white-200 sticky top-0 z-50 flex h-16 w-full items-center justify-between  border border-orange-100 px-5 py-2 text-sm font-medium text-slate-700 backdrop-blur-sm'>
+      <div class='flex items-center'>
+        <a
+          href='/'
+          class='mr-10 flex items-center justify-center font-pacifico text-2xl'
+        >
+          <img
+            class='h-8 mr-2'
+            src={solibeeLogo}
+            alt='the solibee logo: a bee in a honeycomb'
+          />
+          Solibee
+        </a>
+        <span class='mr-6 mt-1 hover:text-orange-200'>
+          <a href='/introduction'>Docs</a>
+        </span>
+        <span class='mt-1 hover:text-orange-200'>
+          <a href={href}>Components</a>
+        </span>
       </div>
-      <hr class='border-1 border-orange-100' />
+      <div class='mt-1 flex items-center'>
+        <Search />
+        <a
+          class='ml-6'
+          target='_blank'
+          href='https://github.com/oslabs-beta/solibee/'
+        >
+          <span class=''>
+            <img class='h-6' src={gitLogo} alt='GitHub logo' />
+          </span>
+        </a>
+        <img class='ml-6 h-6 ' src={sunIcon} alt='Sun icon' />
+      </div>
     </div>
   );
 }
