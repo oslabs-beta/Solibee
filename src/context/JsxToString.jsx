ef3db60c17b1bbd81281057621c0945ced520874
Neul Seo <neul.seo.1213@gmail.com>
2024-04-16 14:53:08 -0400

    rendered code for the switch component

--- src/context/JsxToString.jsx
+++ src/context/JsxToString.jsx
@@ -423,4 +423,25 @@ export default function Accordion() {
     </form>
   );
 }`,
+  Switch: `function Switch({ darkMode, toggleMode }) {
+    return (
+      <div class='justify-center'>
+        <div class='relative'>
+          <div
+            id='switch-container'
+            onClick={toggleMode}
+            class='relative h-7 w-14 cursor-pointer rounded-full bg-gray-300'
+          >
+            {/* in class at the end:  */}
+            <div
+              id='switch-btn'
+              class={\`absolute top-0 h-7 w-7 rounded-full bg-white transition duration-500 ease-in-out \${darkMode() ? 'right-0 bg-slate-500' : 'left-0'}\`}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  export default Switch;`,
 };
