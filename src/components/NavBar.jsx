79f525ee517cc722df5b52ff7e2e4c14b0288048
Neul Seo <neul.seo.1213@gmail.com>
2024-04-16 16:23:27 -0400

    fixed the nav bar for the mode chage

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 // solidJS
-import { useContext } from 'solid-js';
+import { useContext, createEffect, createSignal } from 'solid-js';
 import { ComponentContext } from '../context/ComponentContext';
 
 // assets
@@ -12,6 +12,53 @@ import nightIcon from '../../assets/night-svgrepo-com.svg';
 export default function NavBar() {
   const { components } = useContext(ComponentContext);
   const href = `/component/${components[0].toLowerCase().replaceAll(' ', '')}`;
+  const [isDarkMode, setIsDarkMode] = createSignal(false);
+
+  function handleClick() {
+    setIsDarkMode(!isDarkMode()); // Toggle dark mode
+  }
+
+  // Effect to update icon and theme based on dark mode state
+  createEffect(() => {
+    const modeIcon = document.getElementById('mode-icon');
+    const githubIcon = document.getElementById('github-icon');
+
+    if (isDarkMode()) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      modeIcon.src = nightIcon;
+      modeIcon.alt = 'Night icon';
+      githubIcon.src = gitLogoWhite;
+    } else {
+      document.documentElement.removeAttribute('data-theme');
+      modeIcon.src = sunIcon;
+      modeIcon.alt = 'Sun icon';
+      githubIcon.src = gitLogo;
+    }
+  });
+
+  // function handleClick() {
+  //   const isDarkMode = document.documentElement.hasAttribute('data-theme');
+  //   const modeIcon = document.getElementById('mode-icon');
+  //   const githubIcon = document.getElementById('github-icon');
+
+  //   if (isDarkMode) {
+  //     document.documentElement.removeAttribute('data-theme');
+  //     modeIcon.src = sunIcon;
+  //     modeIcon.alt = 'Sun icon';
+  //     githubIcon.src = gitLogo;
+  //   } else {
+  //     document.documentElement.setAttribute('data-theme', 'dark');
+  //     modeIcon.src = nightIcon;
+  //     modeIcon.alt = 'Night icon';
+  //     githubIcon.src = gitLogoWhite;
+  //   }
+
+  //   modeIcon.removeEventListener('click', handleClick);
+  //   modeIcon.addEventListener('click', handleClick);
+  // }
+
+  // modeIcon.removeEventListener('click', handleClick);
+  // modeIcon.addEventListener('click', handleClick);
 
   return (
     <header class='bg-white-200 sticky top-0 z-50 flex h-16 w-full items-center justify-between border-b border-orange-100 px-5 py-2 text-sm font-medium text-slate-700 backdrop-blur-sm'>
@@ -34,17 +81,29 @@ export default function NavBar() {
           <a href={href}>Components</a>
         </span>
       </div>
-      <div class='mt-1 flex items-center'>
+      <div id='icons' class='mt-1 flex items-center'>
         <a
           class='ml-6'
           target='_blank'
           href='https://github.com/oslabs-beta/solibee/'
         >
           <span class=''>
-            <img class='h-6' src={gitLogo} alt='GitHub logo' />
+            <img
+              id='github-icon'
+              class='h-6'
+              src={isDarkMode() ? gitLogo : gitLogoWhite}
+              alt='GitHub logo'
+            />
           </span>
         </a>
-        <img class='ml-6 h-6 ' src={sunIcon} alt='Sun icon' />
+
+        <img
+          id='mode-icon'
+          class='ml-6 h-6 '
+          src={isDarkMode() ? nightIcon : sunIcon}
+          alt='Sun icon'
+          onClick={handleClick}
+        />
       </div>
     </header>
   );
