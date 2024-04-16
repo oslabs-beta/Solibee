84d66e9d0c89bcaf8d8b0d2d270a8e77a9154ffa
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-16 19:37:33 -0400

    Added some styling to the switch component

--- src/lib/switch/Switch.jsx
+++ src/lib/switch/Switch.jsx
@@ -8,12 +8,12 @@ function Switch({ darkMode, toggleMode }) {
         <div
           id='switch-container'
           onClick={toggleMode}
-          class='relative h-7 w-14 cursor-pointer rounded-full bg-gray-300'
+          class='relative h-7 w-14 cursor-pointer rounded-full bg-orange-100/[0.3]'
         >
           {/* in class at the end:  */}
           <div
             id='switch-btn'
-            class={`absolute top-0 h-7 w-7 rounded-full bg-white transition duration-500 ease-in-out  ${darkMode() ? 'right-0 bg-slate-500' : 'left-0'}`}
+            class={`absolute top-0 h-7 w-7 rounded-full bg-orange-100 transition duration-500 ease-in-out  ${darkMode() ? 'right-0 bg-orange-100' : 'left-0'}`}
           />
         </div>
       </div>
