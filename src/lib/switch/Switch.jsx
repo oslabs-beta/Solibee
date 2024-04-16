92a23ceb496d4187c4bb35ca52e0455d47e89446
tenn501 <158115485+tenn501@users.noreply.github.com>
2024-04-16 16:08:22 -0400

    fix: updated destructured props object in the parameter of Switch component to maximize compatibility with Solid js. Added data-testid for a clickable div to optimize selection during testing

--- src/lib/switch/Switch.jsx
+++ src/lib/switch/Switch.jsx
@@ -1,19 +1,20 @@
 import { createSignal } from 'solid-js';
 
 // eslint-disable-next-line solid/no-destructure
-function Switch({ darkMode, toggleMode }) {
+function Switch(props) {
   return (
     <div class='justify-center'>
       <div class='relative'>
         <div
           id='switch-container'
-          onClick={toggleMode}
+          onClick={() => props.toggleMode()}
           class='relative h-7 w-14 cursor-pointer rounded-full bg-gray-300'
+          data-testid='switch-container'
         >
           {/* in class at the end:  */}
           <div
             id='switch-btn'
-            class={`absolute top-0 h-7 w-7 rounded-full bg-white transition duration-500 ease-in-out  ${darkMode() ? 'right-0 bg-slate-500' : 'left-0'}`}
+            class={`absolute top-0 h-7 w-7 rounded-full bg-white transition duration-500 ease-in-out  ${props.darkMode() ? 'right-0 bg-slate-500' : 'left-0'}`}
           />
         </div>
       </div>
