d625a395850f5a5b5586cdd0c1cf3626ec01c8b5
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-04-16 21:04:54 -0400

    Update dark mode styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './lib/**/*.{js,jsx,ts,tsx}'],
-  darkMode: ['selector', '[data-mode="dark"]'],
+  darkMode: 'selector',
   theme: {
     extend: {
       backgroundImage: {
