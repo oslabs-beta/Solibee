baec05f837ec965f2d3106eef28b9c8492e34ad4
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-17 14:04:35 -0400

    compDescriptions object updated

---
+++ src/context/componentDescriptionObj.js
@@ -0,0 +1,89 @@
+export const compDescriptionsObj = {
+  Accordion: {
+    desc: 'A component that allows users to expand and collapse sections of content.',
+    feats: [
+      'Content hides when collapsed.',
+      'Height animation for every item content.',
+      'Keyboard navigation.',
+    ],
+    inBeta: false,
+    accessibility: 'Follows WAI/ARIA accordion pattern'
+  },
+  DragAndDrop: {
+    desc: 'A component that allows users to drag items from one column to another.',
+    feats: [
+      'Ability to add items per column.',
+      'Items draggable from one column to another.',
+      'Ability to set default items, default columns.',
+    ],
+    inBeta: true, 
+    accessibility: 'Not yet keyboard accessible'
+  },
+  GenerateOTP: {
+    desc: 'A component for generating random one-time passwords(OTPs).',
+    feats: [
+      'Generates and regenerates random OTP.',
+      'Ability to copy and paste generated OTP.',
+    ],
+    inBeta: false, 
+    accessibility: ''
+  },
+  InputFile: {
+    desc: "A component for uploading files selected from users' devices.",
+    feats: [
+      'Ability to add items per column.',
+      'Items draggable from one column to another.',
+      'Ability to set default items, default columns.',
+    ],
+    inBeta: true, 
+    accessibility: ''
+  },
+  InputForm: {
+    desc: 'A form input component that accepts user name and email.',
+    feats: [
+      'Ability to add items per column.',
+      'Items draggable from one column to another.',
+      'Ability to set default items, default columns.',
+    ],
+    inBeta: false, 
+    accessibility:''
+  },
+  InputOTP: {
+    desc: 'A component designed for users to input one-time passwords (OTPs).',
+    feats: [
+      'Ability to add items per column.',
+      'Items draggable from one column to another.',
+      'Ability to set default items, default columns.',
+    ],
+    inBeta: true, 
+    accessibility: ''
+  },
+  SearchButton: {
+    desc: 'A component that allow users to enter search queries and receive relevant page results.',
+    feats: [
+      'Ability to upload one or more files from the device storage of the user.'
+    ],
+    inBeta: true, 
+    accessibility: ''
+  },
+  ToDoList: {
+    desc: 'A component that allows user to update a list of tasks or items that need to be completed.',
+    feats: [
+      'Ability to add items per column',
+      'Items draggable from one column to another',
+      'Ability to set default items, default columns',
+    ],
+    inBeta: true, 
+    accessibility: ''
+  },
+  Switch: {
+    desc: 'A component that toggles between light and dark mode.', 
+    feats: [
+      'Ability to add items per column',
+      'Items draggable from one column to another',
+      'Ability to set default items, default columns',
+    ],
+    inBeta: true, 
+    accessibility: ''
+  },
+};
