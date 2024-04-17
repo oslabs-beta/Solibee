2912a6fa59a49c3e05e41bcefd5483f4d844f579
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-16 23:55:55 -0400

    changed font of steps from font-bold to font-medium

--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -10,7 +10,7 @@ export default function Step(props) {
     <p
       aria-label='Instruction step'
       class='step-counter relative mt-8 pl-10 text-xl
-             font-semibold tracking-tight before:absolute before:left-0 before:z-20 before:flex before:h-[calc(1.375rem+1px)] before:w-[calc(1.375rem+1px)] before:items-center before:justify-center before:rounded-md
+             font-medium tracking-tight before:absolute before:left-0 before:z-20 before:flex before:h-[calc(1.375rem+1px)] before:w-[calc(1.375rem+1px)] before:items-center before:justify-center before:rounded-md
              before:text-[0.9rem] before:font-bold before:text-slate-700 before:shadow-md before:ring-2 before:ring-slate-900/5 before:content-[counter(step)]'
     >
       {props.step}
