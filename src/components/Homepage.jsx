55f3810cab392d20b9009d6086c383ad408ebe67
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2024-04-18 14:25:17 -0400

    Add tool logos to homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,11 @@
 import solibeeLogo from '../../assets/solibee-logo2.png';
 import solibeeLogoDark from '../../assets/solibee-logo-light-v2.png';
+import solidjsLogo from '../../assets/solid.svg';
+import solidjsLogoName from '../../assets/solidjs-light.png';
+import tailwindLogo from '../../assets/tailwindcssLogo.svg';
+import viteLogo from '../../assets/Vite.js.png';
+import jestLogo from '../../assets/jest.svg';
+import npmLogo from '../../assets/npm-logo-red.svg';
 
 export default () => {
   return (
@@ -31,6 +37,27 @@ export default () => {
           <button class='ml-2 mr-2 rounded-xl border-4 border-black bg-orange-100 p-2 pl-5  pr-5 text-xl font-bold hover:border-orange-200 hover:bg-orange-200'>
             See an Example
           </button>
+        </a>
+      </div>
+      <p class='mt-32 text-lg font-bold'>
+        {' '}
+        built open source, with open source tools
+      </p>
+      <a href=''>
+        <img src={solidjsLogoName} class='mt-3 h-16 relative left-2' alt='' />
+      </a>
+      <div class='flex items-center mt-3 justify-center relative right-1'>
+        <a href=''>
+          <img src={tailwindLogo} class='mx-4 h-8' alt='' />
+        </a>
+        <a href=''>
+          <img src={npmLogo} class='mx-4 h-5' alt='' />
+        </a>
+        <a href=''>
+          <img src={viteLogo} class='mx-4 h-8' alt='' />
+        </a>
+        <a href=''>
+          <img src={jestLogo} class='mx-4 h-8' alt='' />
         </a>
       </div>
     </div>
